8064d79811858a71309363ac089d6118170c3870
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-13 12:22:18 -0300

    chore: project setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+
+app.use('/api/v1/tasks', tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
