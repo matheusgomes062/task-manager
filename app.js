9df947be5a4803d261b44253c1d4dcd8ab2ba9f1
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-13 12:44:08 -0300

    feat: add getAllTasks route

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use('/api/v1/tasks', tasks);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
+// const tasks = require('./routes/task')
 
+const { getAllTasks } = require('../controllers/tasks')
+
+router.route('/').get(getAllTasks)
+
+// app.use('/api/v1/tasks', tasks)
 
 module.exports = router
