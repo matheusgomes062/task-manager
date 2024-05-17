94d5269bf92130f14342d35d679899d2b3ea351f
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-17 08:16:09 -0300

    feat: create model

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use('/api/v1/tasks', tasks);
 
 const start = async () => {
   try {
+    await connectDB(process.env.DATABASE_URL);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
