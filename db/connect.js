a5a487e0705f26e2b426c8100b5289db5c68e4cb
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-17 08:03:48 -0300

    feat: database connection to mongo

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,13 @@
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/yourdbname';
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+const connectionString = process.env.DATABASE_URL;
+
+mongoose.connect(connectionString, {
+}).then(() => {
+    console.log('Database connected');
+}).catch((error) => {
+    console.log(error);
+});
